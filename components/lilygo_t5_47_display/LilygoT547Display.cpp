5a735dceb6fe4884e5919762fbf40f300213e09b
Your Name <you@example.com>
2023-02-25 18:59:41 +0100

    Improve and fix LilygoT456 display

--- components/lilygo_t5_47_display/LilygoT547Display.cpp
+++ components/lilygo_t5_47_display/LilygoT547Display.cpp
@@ -8,23 +8,9 @@ namespace lilygo_t5_47_display {
 
 static const char *const TAG = "lilygo_t5_47_display";
 
-float LilygoT547Display::get_setup_priority() const { return esphome::setup_priority::LATE; }
-
-void LilygoT547Display::set_clear_screen(bool clear) { this->clear_ = clear; }
-void LilygoT547Display::set_power_off_delay_enabled(bool power_off_delay_enabled) {
-  this->power_off_delay_enabled_ = power_off_delay_enabled;
-}
-void LilygoT547Display::set_landscape(bool landscape) { this->landscape_ = landscape; }
-
-void LilygoT547Display::set_temperature(uint32_t temperature) { this->temperature_ = temperature; }
-
-int LilygoT547Display::get_width_internal() { return 960; }
-
-int LilygoT547Display::get_height_internal() { return 540; }
-
 void LilygoT547Display::setup() {
   epd_init(EPD_OPTIONS_DEFAULT);
-  hl = epd_hl_init(WAVEFORM);
+  this->hl = epd_hl_init(WAVEFORM);
   if (landscape_) {
     EpdRotation orientation = EPD_ROT_LANDSCAPE;
     epd_set_rotation(orientation);
@@ -32,36 +18,39 @@ void LilygoT547Display::setup() {
     EpdRotation orientation = EPD_ROT_PORTRAIT;
     epd_set_rotation(orientation);
   }
-  fb = epd_hl_get_framebuffer(&hl);
+  this->fb = epd_hl_get_framebuffer(&this->hl);
+  this->partial_updates_ = this->full_update_every_;
 }
 
 void LilygoT547Display::update() {
-  if (this->init_clear_executed_ == false && this->clear_ == true) {
-    LilygoT547Display::clear();
+  epd_poweron();
+  if (this->init_clear_executed_ == false && this->clear_screen_ == true) {
+    this->clear_();
     this->init_clear_executed_ = true;
+  } else {
+    if (this->partial_updates_ == 0 && this->full_update_every_ != 0) {
+      this->clear_();
+    } else {
+      this->partial_updates_--;
+    }
   }
+  // Clear Framebuffer
+  epd_hl_set_all_white(&this->hl);
+
   this->do_update_();
-  LilygoT547Display::flush_screen_changes();
-}
-
-void LilygoT547Display::clear() {
-  epd_poweron();
-  epd_fullclear(&hl, this->temperature_);
-  epd_poweroff();
-}
-
-void LilygoT547Display::flush_screen_changes() {
-  epd_poweron();
-  err = epd_hl_update_screen(&hl, MODE_GC16, this->temperature_);
+  this->flush_screen_changes_();
   if (this->power_off_delay_enabled_ == true) {
     delay(700);
   }
   epd_poweroff();
 }
 
-void LilygoT547Display::set_all_white() { epd_hl_set_all_white(&hl); }
-void LilygoT547Display::poweron() { epd_poweron(); }
-void LilygoT547Display::poweroff() { epd_poweroff(); }
+void LilygoT547Display::clear_() {
+  this->partial_updates_ = this->full_update_every_;
+  epd_fullclear(&this->hl, this->temperature_);
+}
+
+void LilygoT547Display::flush_screen_changes_() { this->err = epd_hl_update_screen(&this->hl, MODE_GC16, this->temperature_); }
 
 void LilygoT547Display::on_shutdown() {
   ESP_LOGI(TAG, "Shutting down Lilygo T5-4.7 screen");
@@ -71,11 +60,11 @@ void LilygoT547Display::on_shutdown() {
 
 void HOT LilygoT547Display::draw_absolute_pixel_internal(int x, int y, Color color) {
   if (color.red == 255 && color.green == 255 && color.blue == 255) {
-    epd_draw_pixel(x, y, 0, fb);
+    epd_draw_pixel(x, y, 0, this->fb);
   } else {
     int col = (0.2126 * color.red) + (0.7152 * color.green) + (0.0722 * color.blue);
     int cl = 255 - col;
-    epd_draw_pixel(x, y, cl, fb);
+    epd_draw_pixel(x, y, cl, this->fb);
   }
 }
 
--- components/lilygo_t5_47_display/LilygoT547Display.h
+++ components/lilygo_t5_47_display/LilygoT547Display.h
@@ -17,47 +17,48 @@ namespace lilygo_t5_47_display {
 
 // LilyGo-EPD47
 class LilygoT547Display : public PollingComponent, public display::DisplayBuffer {
- protected:
-  EpdiyHighlevelState hl;
-  // ambient temperature around device
-  uint8_t *fb;
-  enum EpdDrawError err;
-
  public:
-  float get_setup_priority() const override;
+  float get_setup_priority() const override { return esphome::setup_priority::LATE; }
 
-  void set_clear_screen(bool clear);
-  void set_landscape(bool landscape);
-  void set_power_off_delay_enabled(bool power_off_delay_enabled);
-  void set_temperature(uint32_t temperature);
-
-  int get_width_internal();
-
-  int get_height_internal();
-
-  void setup() override;
-
-  void update() override;
-
-  void clear();
-  void flush_screen_changes();
-  void set_all_white();
-  void poweron();
-  void poweroff();
-  void on_shutdown() override;
-
-#if ESPHOME_VERSION_CODE >= VERSION_CODE(2022,6,0)  
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2022, 6, 0)
   display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
 #endif
+
+  void set_clear_screen(bool val) { this->clear_screen_ = val; }
+  void set_landscape(bool val) { this->landscape_ = val; }
+  void set_power_off_delay_enabled(bool val) { this->power_off_delay_enabled_ = val; }
+  void set_temperature(uint8_t val) { this->temperature_ = val; }
+  void set_full_update_every(uint8_t val) { this->full_update_every_ = val; }
+
+  int get_width_internal() override { return 960; }
+  int get_height_internal() override { return 540; }
+
+  void setup() override;
+  void update() override;
+
+  void set_all_white() { epd_hl_set_all_white(&hl); }
+  void poweron() { epd_poweron(); }
+  void poweroff() { epd_poweroff(); }
+  void on_shutdown() override;
 
  protected:
   void HOT draw_absolute_pixel_internal(int x, int y, Color color) override;
 
-  bool clear_;
+  void clear_();
+  void flush_screen_changes_();
+
   bool init_clear_executed_ = false;
-  bool temperature_;
-  bool power_off_delay_enabled_;
+  EpdiyHighlevelState hl;
+  uint8_t *fb;
+  enum EpdDrawError err;
+
+  bool clear_screen_;
   bool landscape_;
+  bool power_off_delay_enabled_;
+  // ambient temperature around device
+  uint8_t temperature_;
+  uint8_t full_update_every_;
+  uint32_t partial_updates_{0};
 };
 
 }  // namespace lilygo_t5_47_display
