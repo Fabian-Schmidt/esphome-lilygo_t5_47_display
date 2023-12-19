9efc1216dd0a4e66fba5a5fa972470754ce5251b
Your Name <you@example.com>
2023-12-19 21:11:32 +0100

    remove old build flags.

--- components/lilygo_t5_47_display/display.py
+++ components/lilygo_t5_47_display/display.py
@@ -62,5 +62,3 @@ async def to_code(config):
         cg.add_library("https://github.com/Fabian-Schmidt/epdiy.git", None)
     else:
         cg.add_library("https://github.com/vroland/epdiy.git", None)
-    cg.add_build_flag("-DCONFIG_EPD_DISPLAY_TYPE_ED047TC1")
-    cg.add_build_flag("-DCONFIG_EPD_BOARD_REVISION_LILYGO_T5_47")
